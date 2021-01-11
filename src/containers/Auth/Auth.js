da1a116c48a16dfe5cd131bcab6dd332f02748f7
Marko <marko.arsovic@edu.ece.fr$>
2021-01-11 13:51:38 +0100

    added authentication (not operational)

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
-
+import { connect } from 'react-redux';
 import Input from "../../components/UI/Input/Input";
 import Button from "../../components/UI/Button/Button";
+import styles from "./Auth.module.css";
+import * as actions from "../../store/actions/index"
 
 class Auth extends Component {
     state = {
@@ -37,12 +39,59 @@ class Auth extends Component {
         }
     }
 
+    inputChangedHandler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.controls[controlName]),
+                touched: true
+            }
+        };
+        this.setState({controls: updatedControls});
+    }
+
+    checkValidity (value, rules) {
+        let isValid = true;
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if(rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid; 
+        }
+
+        return isValid;
+    }
 
     render() {
-        return (
-            <div>
-                <form>
+        const formElementsArray = [];
+        for (let key in this.state.controls) {
+            formElementsArray.push({
+                id: key,
+                config: this.state.controls[key]
+            })
+        }
 
+        const form = formElementsArray.map(formElement => (
+            <Input
+                key={formElement.id}
+                elementType={formElement.config.elementType}
+                elementConfig={formElement.config.elementConfig}
+                value={formElement.config.value}
+                invalid={!formElement.config.valid}
+                shouldValidate={formElement.config.validation}
+                touched={formElement.config.touched}
+                changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+                
+        ))
+        return (
+            <div className={styles.Auth}>
+                <form>
+                {form}
+                <Button btnType="Success">SUBMIT</Button>
                 </form>
             </div>
         )
@@ -50,4 +99,9 @@ class Auth extends Component {
     }
 }
 
+const madDispacthToProps = dispatch => {
+    return {
+        onAuth: (email, password) => dispatch(actions.auth(email, password))
+    }
+}
 export default Auth;
